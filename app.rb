27919558f452a5302aff5f97efbf50809eb3f38b
Tyler Bristol <tahbristol@gmail.com>
2017-05-11 12:24:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ end
 
 post '/' do
   text_from_user = params[:user_text]
+  @analyzed_text = TextAnalyzer.new(text_from_user)
+  @word_count = @analyzed_text.count_of_words
+  @vowel_count = @analyzed_text.count_of_vowels
+  @non_vowel_count = @analyzed_text.count_of_consonants
+  @most_used_letter = @analyzed_text.most_used_letter
+   @most_used_letter[0] = @most_used_letter[0].upcase
   erb :results
 end
 
